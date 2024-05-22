06e33821b3245f4374e3af67692a1119fac87624
Amin <Torabi.dsd@gmail.com>
2024-05-22 11:35:33 +0330

    Initial Commit

---
+++ A Very big Sum.swift
@@ -0,0 +1,16 @@
+//
+//  A Very big Sum.swift
+//  Algorithm HackerRank
+//
+//  Created by Amir Masoumi on 5/22/24.
+//
+
+import Foundation
+
+public func aVeryBigSum(ar: [Int]) -> Int {
+    var a: Int = 0
+    for int in ar {
+        a += int
+    }
+    return a
+}
---
+++ Algorithm HackerRank/Algorithm_HackerRank.h
@@ -0,0 +1,18 @@
+//
+//  Algorithm_HackerRank.h
+//  Algorithm HackerRank
+//
+//  Created by Amir Masoumi on 5/22/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Algorithm_HackerRank.
+FOUNDATION_EXPORT double Algorithm_HackerRankVersionNumber;
+
+//! Project version string for Algorithm_HackerRank.
+FOUNDATION_EXPORT const unsigned char Algorithm_HackerRankVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algorithm_HackerRank/PublicHeader.h>
+
+
