7a138eeaa35094ee3b1b96d2319ca61f31313bc4
Amin <Torabi.dsd@gmail.com>
2024-05-23 22:24:06 +0330

    count apple and oranges

---
+++ Algorithm HackerRankTests/countApplesAndOrangesTests.swift
@@ -0,0 +1,21 @@
+//
+//  countApplesAndOrangesTests.swift
+//  Algorithm HackerRankTests
+//
+//  Created by Amir Masoumi on 5/23/24.
+//
+/// https://www.hackerrank.com/challenges/apple-and-orange
+
+import XCTest
+import Algorithm_HackerRank
+
+final class countApplesAndOrangesTests: XCTestCase {
+
+    func testExample() throws {
+        let counts =  countApplesAndOranges(s: 7, t: 10, a: 4, b: 12, apples: [2, 3, -4], oranges: [3, -2, -4])
+
+        XCTAssertEqual(counts.apples, 1)
+        XCTAssertEqual(counts.oranges, 2)
+    }
+
+}
---
+++ countApplesAndOranges.swift
@@ -0,0 +1,28 @@
+//
+//  countApplesAndOranges.swift
+//  Algorithm HackerRank
+//
+//  Created by Amir Masoumi on 5/23/24.
+//
+
+import Foundation
+
+
+public func countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: [Int], oranges: [Int]) -> (apples: Int, oranges: Int) {
+
+    let applesLocations = apples.map({ $0 + a})
+    let orangesLocations = oranges.map({ $0 + b})
+
+    var numberOfApples = 0
+    var numberOfOranges = 0
+
+    applesLocations.forEach({ if  s <= $0 && $0 <= t {
+        numberOfApples += 1
+    }})
+
+    orangesLocations.forEach({ if  s <= $0 && $0 <= t {
+        numberOfOranges += 1
+    }})
+
+    return (numberOfApples, numberOfOranges)
+}
