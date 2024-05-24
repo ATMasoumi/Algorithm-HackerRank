55444ed511d2e36e580be139ea485d49073a0f9e
Amin <Torabi.dsd@gmail.com>
2024-05-24 13:01:51 +0330

    finding factors

---
+++ Algorithm HackerRankTests/FindingFactorsTests.swift
@@ -0,0 +1,34 @@
+//
+//  FindingFactorsTests.swift
+//  Algorithm HackerRankTests
+//
+//  Created by Amir Masoumi on 5/24/24.
+//
+/// https://www.hackerrank.com/challenges/between-two-sets
+
+import XCTest
+import Algorithm_HackerRank
+
+final class FindingFactorsTests: XCTestCase {
+
+    func testExample() throws {
+        let a = [2,6]
+        let b = [24, 36]
+        let total = getTotalX(a: a, b: b)
+        XCTAssertEqual(total, 2)
+    }
+
+    func testExample2() throws {
+        let a = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91]
+        let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        let total = getTotalX(a: a, b: b)
+        XCTAssertEqual(total, 0)
+    }
+
+    func testExample3() throws {
+        let a = [51]
+        let b = [50]
+        let total = getTotalX(a: a, b: b)
+        XCTAssertEqual(total, 0)
+    }
+}
---
+++ FindingFactors .swift
@@ -0,0 +1,39 @@
+//
+//  FindingFactors .swift
+//  Algorithm HackerRank
+//
+//  Created by Amir Masoumi on 5/24/24.
+//
+
+import Foundation
+
+public func getTotalX(a: [Int], b: [Int]) -> Int {
+    // Write your code here
+    guard let aMax = a.max(),
+          let bMin = b.min(), aMax < bMin  else { return 0}
+
+    var numberOfValidItems = 0
+    for i in aMax...bMin {
+        // first condition
+        var isFirstConditionValid = true
+        for int in a {
+            if i%int != 0 {
+                isFirstConditionValid = false
+            }
+        }
+
+        // second condition
+        var isSecondConditionValid = true
+        for int in b {
+            if int%i != 0 {
+                isSecondConditionValid = false
+            }
+        }
+
+        if isFirstConditionValid && isSecondConditionValid {
+            numberOfValidItems += 1
+        }
+    }
+    return numberOfValidItems
+
+}
